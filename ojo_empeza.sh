bd21d0207bfb6a5014d8c5971da81c77d553c97a
Michael Budnick <michael.budnick@outlook.com>
2019-03-17 19:23:07 -0400

    Picture taken at 03/17/19 07:23:00 PM

--- ojo_empeza.sh
+++ ojo_empeza.sh
@@ -3,12 +3,13 @@ while true; do
 	pic_time=$(date '+%D %X')
 	echo "Taking picture at: $pic_time"
 	SECONDS=0
+        # Call python to take picture and save
 	python3 ./ojo_controlador.py
-	until [ $SECONDS -gt 15 ]; do
-		dummy_variable=true
-	done
-        echo "Pushing to git at: $(date '+%D %X')"
-	git add .
+	
+        git add .
 	git commit -a -m "Picture taken at $pic_time"
 	git push origin dev
+        until [ $SECONDS -gt 300 ]; do
+		dummy_variable=true
+	done
 done

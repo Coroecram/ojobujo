14ad0984f968639bac1055b115bad73b8907b7fa
Michael Budnick <michael.budnick@outlook.com>
2019-03-20 22:34:24 -0400

    MVP

---
+++ ojobujo_surveillance_system.py
@@ -0,0 +1,97 @@
+# Web streaming example
+# Source code from the official PiCamera package
+# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
+
+import io
+import picamera
+import logging
+import socketserver
+from threading import Condition
+from http import server
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            with open('index.html' , "r") as PAGE:
+                content = PAGE.read().replace('\n', '').encode('utf-8')
+            PAGE.close()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/assets/styles.css':
+            with open('./assets/styles.css' , "r") as STYLES:
+                styles = STYLES.read().replace('\n', '').encode('utf-8')
+            STYLES.close()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/css')
+            self.send_header('Content-Length', len(styles))
+            self.end_headers()
+            self.wfile.write(styles)
+        elif self.path == '/assets/ojobujo_64.ico':
+            with open('./assets/ojobujo_64.ico' , "r") as ICON:
+                ICON = ICON.read()
+            ICON.close()
+            self.send_response(200)
+            self.send_header('Content-Type', 'image/x-icon')
+            self.send_header('Content-Length', len(icon))
+            self.end_headers()
+            self.wfile.write(icon)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+with picamera.PiCamera(resolution='320x240', framerate=12) as camera:
+    output = StreamingOutput()
+    camera.vflip = True
+    camera.start_recording(output, format='mjpeg')
+    try:
+        address = ('', 80)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
